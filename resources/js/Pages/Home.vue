2add0324cdc22e4e3062ab33dab8a8d8d50e7cb1
crystal <crystalbritish1120@gmail.com>
2023-04-27 02:17:58 +0900

    init

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="w-full min-h-screen bg-orange-100 flex items-center justify-center">
+        <span class="text-7xl text-sky-600 font-bold">
+            Maintanance
+        </span>
+    </div>
+</template>
